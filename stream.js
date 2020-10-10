1cd4ec10fa7ba384db7928c9824edbbf8eb7c53c
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-10 08:39:14 +0200

    removing multiple stream

--- stream.js
+++ stream.js
@@ -1,11 +1,8 @@
 var options = {
-    width: 640,
-    height: 360,
+    width: 1280,
+    height: 720,
     channel: "jeremysadi",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
     parent: ["zevent.camille-bessancourt.fr"]
   };
-  new Twitch.Player("streamZ", options)
-
-  options.channel = "shock_rl"
   new Twitch.Player("stream", options)
