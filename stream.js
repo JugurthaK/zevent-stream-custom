65d57e783a7908cc2467ae48ae70154b16b13b5d
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-10 08:14:21 +0200

    First commit

---
+++ stream.js
@@ -0,0 +1,9 @@
+var options = {
+    width: 1280,
+    height: 720,
+    channel: "jeremysadi",
+    // only needed if your site is also embedded on embed.example.com and othersite.example.com 
+    parent: ["zevent.github.io"]
+  };
+  var player = new Twitch.Player("stream", options);
+  player.setVolume(0.5);
