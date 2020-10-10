db65b413e57086514357cd27aa5ebe30cc5e436d
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-10 08:35:19 +0200

    double stream frontpage handling

--- stream.js
+++ stream.js
@@ -1,9 +1,11 @@
 var options = {
-    width: 1280,
-    height: 720,
+    width: 640,
+    height: 360,
     channel: "jeremysadi",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
     parent: ["zevent.camille-bessancourt.fr"]
   };
-  var player = new Twitch.Player("stream", options);
-  player.setVolume(0.5);
+  new Twitch.Player("streamZ", options)
+
+  options.channel = "shock_rl"
+  new Twitch.Player("stream", options)
