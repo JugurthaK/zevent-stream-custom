9162e191ac9c2be2ced75c081f466f90e73e7f03
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-10 09:41:27 +0200

    Initial streamer update

--- stream.js
+++ stream.js
@@ -1,7 +1,7 @@
 var options = {
     width: 1280,
     height: 720,
-    channel: "jeremysadi",
+    channel: "zerator",
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
     parent: ["zevent.camille-bessancourt.fr"]
   };
