d8fde093c57fb3cd030e6a1c5dc9b508a873eaa3
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-10 09:36:41 +0200

    Selector updater handled

--- stream.js
+++ stream.js
@@ -5,4 +5,11 @@ var options = {
     // only needed if your site is also embedded on embed.example.com and othersite.example.com 
     parent: ["zevent.camille-bessancourt.fr"]
   };
-  let player = new Twitch.Player("stream", options)
+let player = new Twitch.Player("stream", options)
+
+document.getElementById("update").addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let streamer = document.getElementById("streamSelector").value
+    player.setChannel(streamer)
+})
