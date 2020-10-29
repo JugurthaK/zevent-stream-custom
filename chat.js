809641b52afbd680c6ca364d62d1bb75c561a434
Jugurtha Kabeche <jugurtha.kabeche@orange.com>
2020-10-29 19:21:03 +0100

    chat hidding update

--- chat.js
+++ chat.js
@@ -1,5 +1,7 @@
 let updateChat = () => {
     options.layout === "video" ? options.layout = "video-with-chat" : options.layout = "video"
+
+    document.getElementById("stream").firstChild.remove()
     player = new Twitch.Embed("stream", options);
 }
 
