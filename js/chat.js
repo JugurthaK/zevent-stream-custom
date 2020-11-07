61c74d71108a743a836942735a7c9a54b1511f61
jugurthak <jugurtha.halzoun@gmail.com>
2020-11-07 20:20:19 +0100

    Hide chat button

--- js/chat.js
+++ js/chat.js
@@ -14,3 +14,21 @@ let updateChat = (streamer) => {
     let iframe = document.getElementById("chatIframe")
     iframe.setAttribute('src',`https://www.twitch.tv/embed/${streamer}/chat?parent=zevent.camille-bessancourt.fr&darkpopout`)
 }
+
+let hideChat = () => {
+    let chat = document.getElementById("chat");
+    let stream = document.getElementById("stream");
+
+    let hidden = false
+    if (chat.style.display === 'none')
+        hidden = true
+
+    hidden ? chat.style.display = 'block' : chat.style.display = 'none'
+    hidden ? stream.classList.replace("col-10", "col-8") : stream.classList.replace("col-8", "col-10")
+}
+
+document.getElementById("buttonChat").addEventListener("click", e => {
+    e.preventDefault()
+
+    hideChat();
+})
--- js/favorite.js
+++ js/favorite.js
@@ -24,4 +24,3 @@ if (favoriteStreamers[0] !== undefined) {
         });
     }
 }
-
